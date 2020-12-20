3b87798917dbae20ebc83fe39a56cf6105220ccd
Snow-Sudo <69723902+Snow-Sudo@users.noreply.github.com>
2020-12-20 15:05:36 -0500

    Add files via upload

--- i3/set_monitor.sh
+++ i3/set_monitor.sh
@@ -1,4 +1,4 @@
-
 #!/usr/bin/env sh
-xrandr --output DP-4 --primary --mode 1920x1080 --rate 144 --output DP-2 --right-of DP-4 --mode 1920x1080 --rate 144
-xinput --set-prop 9 301 -0.65
+xrandr --output DP-2 --primary --mode 1920x1080 --rate 144 --left-of DP-4
+xrandr --output DP-4  --mode 1920x1080 --rate 144 --right-of DP-2
+xinput --set-prop 9 298 -0.70
