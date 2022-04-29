eadd60edcc1c6654f18be02c71a046b12738ca1f
Max Liu <69723902+Snow-0@users.noreply.github.com>
2022-04-29 21:52:37 +0000

    Update keymaps.lua

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -31,6 +31,9 @@ keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
 
+-- format 
+keymap("n", "<C-f>", ":Format<CR>", opts)
+
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
