f6032743ed49b6c289fb08944bf57cb04bcf3fd2
Max Liu <69723902+Snow-0@users.noreply.github.com>
2023-06-13 02:08:20 +0000

    Delete i3 directory

--- i3/set_monitor.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-xrandr --output DP-2 --primary --mode 1920x1080 --rate 144 --left-of DP-4
-xrandr --output DP-4  --mode 1920x1080 --rate 144 --right-of DP-2
-xinput --set-prop 9 298 -0.70
